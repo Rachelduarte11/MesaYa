b6be5297654d81f529ec2a5e3ec9321f87940b83
Esclender <elesclenderlugo@gmail.com>
2025-04-13 18:40:54 -0500

    feat: main route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,10 @@ import { CategoryFilter } from "../components/category-filter"
 import { FoodGrid } from "../components/food-grid"
 import { Cart } from "../components/cart"
 import { Footer } from "../components/footer"
+import OrderPage from "./order/page"
 
 export default function POSPage() {
   return (
-    <div className="flex h-screen bg-gray-100">
-      <SidebarNav />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        {/* <Header /> */}
-        <div className="flex-1 flex overflow-hidden">
-          <main className="flex-1 overflow-auto p-4">
-            <CategoryFilter />
-            <FoodGrid />
-          </main>
-          <Cart />
-        </div>
-      </div>
-    </div>
+    <OrderPage />
   )
 }
